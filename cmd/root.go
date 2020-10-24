ccac3f22bbae69c74f6ca8aec2005705d0512853
Filip Krakowski <krakowski@hhu.de>
2020-10-24 13:29:58 +0200

    Bump version to 1.3.8

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	version = "1.3.7"
+	version = "1.3.8"
 	releaseUrl = "https://api.github.com/repos/krakowski/ilias-cli/releases/latest"
 	downloadUrlTemplate = "https://github.com/krakowski/ilias-cli/releases/download/%s/ilias-%s-%s"
 )
