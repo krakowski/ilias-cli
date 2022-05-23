1d4718d20cf04ad878271a4fa2720472c56f19ac
Filip Krakowski <krakowski@hhu.de>
2022-05-23 12:05:58 +0200

    Remove limit on number of rows in exercise list

--- cmd/root.go
+++ cmd/root.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	version = "1.4.7"
+	version = "1.4.8"
 	releaseUrl = "https://api.github.com/repos/krakowski/ilias-cli/releases/latest"
 	downloadUrlTemplate = "https://github.com/krakowski/ilias-cli/releases/download/%s/ilias-%s-%s"
 )
