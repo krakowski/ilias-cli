e95400d13aa974aff1d794ec9e5b5e340410d0ec
Filip Krakowski <krakowski@hhu.de>
2023-04-27 10:47:09 +0200

    Update for ILIAS v7.20

--- cmd/root.go
+++ cmd/root.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	version = "1.4.8"
+	version = "1.4.9"
 	releaseUrl = "https://api.github.com/repos/krakowski/ilias-cli/releases/latest"
 	downloadUrlTemplate = "https://github.com/krakowski/ilias-cli/releases/download/%s/ilias-%s-%s"
 )
